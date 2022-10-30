b783f370efd4bf7742e11cd580f5750bd248234d
SHOUKAT <shoukatrahman2001@gmail.com>
2022-10-30 12:19:24 -0700

    data

---
+++ Connecting SQL/Connecting SQL/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace Connecting_SQL
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txt_RgdID = new System.Windows.Forms.TextBox();
+            this.txt_Name = new System.Windows.Forms.TextBox();
+            this.txt_Mobile = new System.Windows.Forms.TextBox();
+            this.txt_Rno = new System.Windows.Forms.TextBox();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.txt_Fname = new System.Windows.Forms.TextBox();
+            this.txt_Address = new System.Windows.Forms.TextBox();
+            this.btn_Save = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Register Student";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "RegisterID";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(59, 118);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(58, 199);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Mobile";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(59, 166);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(68, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Father Name";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(64, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Rno";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(64, 273);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(32, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Email";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(59, 312);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(45, 13);
+            this.label8.TabIndex = 8;
+            this.label8.Text = "Address";
+            // 
+            // txt_RgdID
+            // 
+            this.txt_RgdID.Location = new System.Drawing.Point(173, 66);
+            this.txt_RgdID.Name = "txt_RgdID";
+            this.txt_RgdID.Size = new System.Drawing.Size(100, 20);
+            this.txt_RgdID.TabIndex = 9;
+            // 
+            // txt_Name
+            // 
+            this.txt_Name.Location = new System.Drawing.Point(173, 111);
+            this.txt_Name.Name = "txt_Name";
+            this.txt_Name.Size = new System.Drawing.Size(100, 20);
+            this.txt_Name.TabIndex = 10;
+            // 
+            // txt_Mobile
+            // 
+            this.txt_Mobile.Location = new System.Drawing.Point(173, 192);
+            this.txt_Mobile.Name = "txt_Mobile";
+            this.txt_Mobile.Size = new System.Drawing.Size(100, 20);
+            this.txt_Mobile.TabIndex = 11;
+            // 
+            // txt_Rno
+            // 
+            this.txt_Rno.Location = new System.Drawing.Point(173, 233);
+            this.txt_Rno.Name = "txt_Rno";
+            this.txt_Rno.Size = new System.Drawing.Size(100, 20);
+            this.txt_Rno.TabIndex = 12;
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(173, 266);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(100, 20);
+            this.txt_Email.TabIndex = 13;
+            // 
+            // txt_Fname
+            // 
+            this.txt_Fname.Location = new System.Drawing.Point(173, 159);
+            this.txt_Fname.Name = "txt_Fname";
+            this.txt_Fname.Size = new System.Drawing.Size(100, 20);
+            this.txt_Fname.TabIndex = 14;
+            // 
+            // txt_Address
+            // 
+            this.txt_Address.Location = new System.Drawing.Point(173, 305);
+            this.txt_Address.Multiline = true;
+            this.txt_Address.Name = "txt_Address";
+            this.txt_Address.Size = new System.Drawing.Size(100, 67);
+            this.txt_Address.TabIndex = 15;
+            // 
+            // btn_Save
+            // 
+            this.btn_Save.Location = new System.Drawing.Point(184, 391);
+            this.btn_Save.Name = "btn_Save";
+            this.btn_Save.Size = new System.Drawing.Size(75, 23);
+            this.btn_Save.TabIndex = 16;
+            this.btn_Save.Text = "Save";
+            this.btn_Save.UseVisualStyleBackColor = true;
+            this.btn_Save.Click += new System.EventHandler(this.btn_Save_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(378, 450);
+            this.Controls.Add(this.btn_Save);
+            this.Controls.Add(this.txt_Address);
+            this.Controls.Add(this.txt_Fname);
+            this.Controls.Add(this.txt_Email);
+            this.Controls.Add(this.txt_Rno);
+            this.Controls.Add(this.txt_Mobile);
+            this.Controls.Add(this.txt_Name);
+            this.Controls.Add(this.txt_RgdID);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txt_RgdID;
+        private System.Windows.Forms.TextBox txt_Name;
+        private System.Windows.Forms.TextBox txt_Mobile;
+        private System.Windows.Forms.TextBox txt_Rno;
+        private System.Windows.Forms.TextBox txt_Email;
+        private System.Windows.Forms.TextBox txt_Fname;
+        private System.Windows.Forms.TextBox txt_Address;
+        private System.Windows.Forms.Button btn_Save;
+    }
+}
+
---
+++ Connecting SQL/Connecting SQL/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Connecting_SQL
+{
+    public partial class Form1 : Form
+    {
+        string ConString = System.Configuration.ConfigurationManager.ConnectionStrings["myConString"].ToString();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Save_Click(object sender, EventArgs e)
+        {
+            string sqlQuery = "INSERT INTO tbl_student VALUES('" + txt_RgdID.Text + "','" + txt_Name.Text + "','" + txt_Fname.Text + "','" + txt_Mobile.Text + "','" + txt_Rno.Text + "','" + txt_Email.Text + "','" + txt_Address.Text + "')";
+            SqlConnection con = new SqlConnection(ConString);
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sqlQuery, con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
